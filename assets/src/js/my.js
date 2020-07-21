0b0101a36c6996184ea16aa8398db80a2246e459
R1bove <97alexxxey97@gmail.com>
2020-07-22 00:34:23 +0300

    add create cardRestaurant and foodCard

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // modal cart
 
 const close_cart  = document.querySelector(".close-modal_cart");
@@ -7,17 +9,17 @@ const overlay = document.querySelector("body");
 
 cartBtn.addEventListener("click", function (event) {
     modal_cart.classList.add("is-open");
-    overlay.classList.add("hidden");
+    overlay.classList.add("o-hidden");
 })
 close_cart.addEventListener("click", function (event) {
     modal_cart.classList.remove("is-open");
-    overlay.classList.remove("hidden");
+    overlay.classList.remove("o-hidden");
 })
 
 
 // pajax container
 
-$(document).pjax('.restaurant__cards_card a', '.pjax-container', {fragment: '.pjax-container'});
+// $(document).pjax('.restaurant__cards_card a', '.pjax-container', {fragment: '.pjax-container'});
 
 
 // modal-auth
@@ -29,7 +31,10 @@ const logInForm = document.querySelector("#authForm");
 const loginInput = document.querySelector("#login-field");
 let  userName = document.querySelector(".user-name");
 let login = localStorage.getItem('dfood');
-
+//cards
+const cardsRestaurants = document.querySelector('#restaurant__cards');
+const cardsFoodRestaurant = document.querySelector('#food__cards');
+let newcard = localStorage.getItem("card");
 
 function authorized() {
     console.log("авторизован");
@@ -81,4 +86,73 @@ function checkAuth() {
     }
 }
 checkAuth();
+
+
+//create card
+
+function createCardRestaurant() {
+    const card = `<div class="restaurant__cards_card card animated zoomInDown">
+                            <a>
+                                <img src="../img/main/cards/tanuki.jpg" alt="">
+                                <div class="card__info">
+                                    <div class="card__info_headline">
+                                        <h3 class="card__title">Тануки</h3>
+                                        <span class="time">50 мин</span>
+                                    </div>
+                                    <div class="card__info_short-desc">
+                                        <span class="rate"><i class ="fas fa-star"></i>4.5</span>
+                                        <span class="price">От 900 ₽ </span>
+                                        <span class="type">Пицца </span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>`;
+    cardsRestaurants.insertAdjacentHTML('beforeend', card);
+}
+if (cardsRestaurants) {
+    createCardRestaurant();
+    cardsRestaurants.addEventListener('click', openGoods);
+}
+
+function createCardGood() {
+    const foodCard = `<div class="restaurant__cards_card card animated zoomInLeft">
+                            <img src="../img/main/cards-menu/roll-ugor-standart.jpg" alt="">
+                            <div class="card__info">
+                                <div class="card__info_headline card__info_headline-menu">
+                                    <h3 class="card__title">Пицца плюс</h3>
+                                </div>
+                                <div class="card__info_short-desc">
+                                    <span class="desc">Рис, угорь, соус унаги, кунжут, водоросли нори. </span>
+                                </div>
+                                <div class="card-footer">
+                                    <button class="btn btn-primary">
+                                        В корзину
+                                        <i class="far fa-shopping-cart"></i>
+                                    </button>
+                                    <div class="price">
+                                        <span>250 ₽</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+    cardsFoodRestaurant.insertAdjacentHTML('beforeend', foodCard);
+    localStorage.removeItem("card");
+}
+
+
+if (newcard) {
+    document.addEventListener("DOMContentLoaded", function() {
+        createCardGood();
+    });
+}
+
+
+function openGoods(event) {
+    const typeRestaurant = event.target.closest("a");
+    localStorage.setItem('card', typeRestaurant);
+    console.log(typeRestaurant);
+    if (typeRestaurant) {
+        // document.location.href = "/restaurant-shop.html";
+    }
+}
 
